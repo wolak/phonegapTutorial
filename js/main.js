aba62da1539bbe7f8fb99d9b28f91bb359b307ff
wolakpaul <wolakpaul@gmail.com>
2015-11-07 14:52:27 -0700

    adds a view class and css

--- js/main.js
+++ js/main.js
@@ -1,23 +1,9 @@
 var app = {
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -25,7 +11,7 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-    }
+    },
 
 };
 
